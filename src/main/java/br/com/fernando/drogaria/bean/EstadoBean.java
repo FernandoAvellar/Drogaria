24f0d611b60aed6a1eac0641c1ee11a13ce0cb78
Fernando Octávio Avellar Jr <fernando@MacBook-Air-de-Fernando.local>
2016-06-28 23:07:38 -0300

    Apos aula 48

--- src/main/java/br/com/fernando/drogaria/bean/EstadoBean.java
+++ src/main/java/br/com/fernando/drogaria/bean/EstadoBean.java
@@ -1,14 +1,41 @@
 package br.com.fernando.drogaria.bean;
 
+import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import org.omnifaces.util.Messages;
+import br.com.fernando.drogaria.dao.EstadoDAO;
+import br.com.fernando.drogaria.domain.Estado;
 
+@SuppressWarnings("serial")
 @ManagedBean //Anotação pra dizer que é uma classe que cuidará do controle e do modelo dentro do nosso projeto
-public class EstadoBean {
+@ViewScoped //Os objetos ficarão validos enquanto estiver na tela do estado correspondente
+public class EstadoBean implements Serializable {
 	
-	public void salvar(){
-
-		Messages.addGlobalInfo("Programação Web com Java");
+	private Estado estado;
+	
+	public Estado getEstado() {
+		return estado;
 	}
 
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public void novo(){
+		estado = new Estado();
+	}
+	
+	public void salvar(){	
+		try {
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estadoDAO.salvar(estado);
+			novo(); //Limpa o objeto
+			Messages.addGlobalInfo("Estado salvo com sucesso");
+		}catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao salvar o estado");
+			erro.printStackTrace();
+		}
+	}
+	
 }
